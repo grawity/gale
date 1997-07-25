470dedee020567e38063eaf128ae4a61c69e582c
Dan Egnor <egnor@ofb.net>
1997-07-25 04:45:56 +0000

    Changed interface.

--- include/gale/auth.h
+++ include/gale/auth.h
@@ -1,22 +1,19 @@
-#ifndef AUTH_H
-#define AUTH_H
+#ifndef GALE_AUTH_H
+#define GALE_AUTH_H
 
 #include "message.h"
 
 void gale_keys(void);
 
-char *sign_data(const char *id,const char *data,const char *end);
 char *verify_data(const char *sig,const char *data,const char *end);
 
 #define ENCRYPTION_PADDING 8
 #define DECRYPTION_PADDING 8
 
-char *encrypt_data(const char *id,const char *data,const char *dend,
-                   char *out,char **oend);
 char *decrypt_data(char *header,const char *data,const char *end,
                    char *out,char **oend);
 
-void sign_message(const char *id,struct gale_message *);
-void encrypt_message(const char *id,struct gale_message *);
+struct gale_message *sign_message(const char *id,struct gale_message *);
+struct gale_message *encrypt_message(const char *id,struct gale_message *);
 
 #endif
