6ce667fd3ad809e1396109b9907668e1ca4cf19e
Dan Egnor <egnor@ofb.net>
2000-01-28 04:56:50 +0000

    boom

--- liboop/foreign/HTReq.h
+++
@@ -1,8 +0,0 @@
-/* HTReq.h spoof -- exists only to make HTEvent.h work */
-
-#ifndef HTREQ_H_SPOOF
-#define HTREQ_H_SPOOF
-
-typedef struct HTRequest_spoof HTRequest;
-
-#endif
--- liboop/foreign/wwwsys.h
+++
@@ -1,9 +0,0 @@
-/* wwwsys.h spoof -- exists only to make HTEvent.h work */
-
-#ifndef WWWSYS_H_SPOOF
-#define WWWSYS_H_SPOOF
-
-typedef char BOOL;
-typedef int SOCKET;
-
-#endif
