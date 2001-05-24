22d301ff5188adb32f35eceacea4355a72c35545
Dan Egnor <egnor@ofb.net>
2001-05-24 23:40:20 +0000

    Initial stab

--- gdomain/gdomain.c
+++ gdomain/gdomain.c
@@ -1,86 +1,65 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <time.h>
-
 #include "gale/all.h"
 
-struct gale_text category;
-struct auth_id *domain;
+#include <stdlib.h>
 
-void usage() {
+static oop_source *source;
+static struct gale_link *line;
+static struct gale_text subscriptions;
+
+static void *on_error_packet(struct gale_packet *pkt,void *x) {
+	link_put(line,pkt);
+	return OOP_CONTINUE;
+}
+
+static void *on_error_message(struct gale_message *msg,void *x) {
+	gale_pack_message(source,msg,on_error_packet,x);
+	return OOP_CONTINUE;
+}
+
+static void *on_connected(struct gale_server *server,
+	struct gale_text host,
+	struct sockaddr_in addr,void *x)
+{
+	struct gale_error_queue *queue = gale_make_queue(source);
+	gale_on_queue(queue,on_error_message,line);
+
+	gale_daemon(source);
+	gale_kill(gale_var(G_("GALE_DOMAIN")),1);
+	gale_detach();
+
+	if (0 != subscriptions.l) link_subscribe(line,subscriptions);
+	return OOP_CONTINUE;
+}
+
+static void *on_packet(struct gale_link *link,struct gale_packet *pkt,void *x) {
+	/* ... process (and respond to) message ... */
+	return OOP_CONTINUE;
+}
+
+static void *on_query_location(struct gale_text name,
+	struct gale_location *loc,void *x) 
+{
+	struct gale_location *list[2] = { loc, NULL };
+	subscriptions = gale_pack_subscriptions(list,NULL);
+	link_subscribe(line,subscriptions);
+	return OOP_CONTINUE;
+}
+
+static void usage() {
 	fprintf(stderr,"%s\n"
-	"usage: gdomain [-h]\n"
+	"usage: gdomain [-h] [domain]\n"
 	"flags: -h          Display this message\n",GALE_BANNER);
 	exit(1);
 }
 
-struct old_gale_message *slip(struct auth_id *id,struct gale_fragment frag) {
-	struct old_gale_message *msg = gale_make_message();
-	gale_add_id(&msg->data,null_text);
-	gale_group_add(&msg->data,frag);
-	msg->cat = id_category(id,G_("auth/key"),G_(""));
-	return msg;
-}
-
-struct old_gale_message *success(struct auth_id *id) {
-	struct gale_fragment frag;
-	frag.name = G_("answer/key");
-	frag.type = frag_data;
-	export_auth_id(id,&frag.value.data,0);
-	return slip(id,frag);
-}
-
-struct old_gale_message *failure(struct auth_id *id) {
-	struct gale_fragment frag;
-	frag.name = G_("answer/key/error");
-	frag.type = frag_text;
-	frag.value.text = gale_text_concat(3,
-		G_("key \""),auth_id_name(id),G_("\" not found"));
-	return slip(id,frag);
-}
-
-void request(struct gale_link *link,struct auth_id *id) {
-	struct old_gale_message *reply;
-	gale_check_mem();
-	reply = auth_id_public(id) ? success(id) : failure(id);
-	auth_sign(&reply->data,domain,AUTH_SIGN_SELF);
-	if (reply) link_put(link,gale_transmit(reply));
-}
-
-void *on_message(struct gale_link *link,struct gale_packet *pkt,void *data) {
-	struct old_gale_message *msg = gale_receive(pkt);
-	struct auth_id *encrypted = NULL,*signature = NULL;
-	struct gale_fragment frag;
-
-	gale_check_mem();
-
-	encrypted = auth_decrypt(&msg->data);
-	if (!msg) return OOP_CONTINUE;
-	signature = auth_verify(&msg->data);
-
-	if (!gale_group_lookup(msg->data,G_("question/key"),frag_text,&frag))
-		gale_alert(GALE_WARNING,G_("cannot determine the key wanted"),0);
-	else {
-		struct auth_id *key;
-		init_auth_id(&key,frag.value.text);
-		request(link,key);
-	}
-
-	return OOP_CONTINUE;
-}
-
 int main(int argc,char *argv[]) {
-	struct gale_link *link;
-	struct gale_server *server;
 	oop_source_sys *sys;
-	oop_source *source;
+	struct gale_server *server;
 	int arg;
 
 	gale_init("gdomain",argc,argv);
 	gale_init_signals(source = oop_sys_source(sys = oop_sys_new()));
-	disable_gale_akd();
+	subscriptions = null_text;
 
 	while ((arg = getopt(argc,argv,"dDh")) != EOF)
 	switch (arg) {
@@ -89,22 +68,15 @@ int main(int argc,char *argv[]) {
 	case 'h':
 	case '?': usage();
 	}
+
 	if (optind != argc) usage();
 
-	init_auth_id(&domain,gale_var(G_("GALE_DOMAIN")));
-	if (!domain || !auth_id_private(domain))
-		gale_alert(GALE_ERROR,G_("no access to domain private key"),0);
+	line = new_link(source);
+	link_on_message(line,on_packet,NULL);
+	server = gale_make_server(source,line,null_text,0);
+	gale_on_connect(server,on_connected,NULL);
+	gale_find_location(source,G_("_gale.query"),on_query_location,NULL);
 
-	category = dom_category(auth_id_name(domain),G_("auth/query"));
-	link = new_link(source);
-	server = gale_open(source,link,category,null_text,0);
-	gale_set_error_link(source,link);
-
-	gale_daemon(source);
-	gale_kill(auth_id_name(domain),1);
-	gale_detach();
-	link_on_message(link,on_message,NULL);
 	oop_sys_run(sys);
-
 	return 0;
 }
