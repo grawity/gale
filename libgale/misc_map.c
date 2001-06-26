a59ddec157a6fb775f5b095a5f960f54ee873c59
Dan Egnor <egnor@ofb.net>
2001-06-26 19:35:53 +0000

    Better memory management

--- libgale/misc_map.c
+++ libgale/misc_map.c
@@ -21,11 +21,32 @@ struct gale_map *gale_make_map(int weak) {
 	return wt;
 }
 
+static struct wt_node *distill(struct wt_node *node) {
+	struct wt_node **parent,*temp;
+
+	if (NULL == node) return NULL;
+	if (NULL != gale_get_ptr(node->data)) return node;
+	if (NULL == (node->right = distill(node->right)))
+		return distill(node->left);
+	if (NULL == (node->left = distill(node->left)))
+		return node->right;
+
+	parent = &node->left;
+	while (NULL != (*parent)->right)
+		parent = &(*parent)->right;
+
+	temp = *parent;
+	*parent = temp->left;
+	temp->left = node->left;
+	temp->right = node->right;
+	return distill(temp);
+}
+
 static struct wt_node **find(struct gale_map *wt,struct gale_data key) {
 	struct wt_node **p = &wt->root;
-	while (*p) {
-		int x = gale_data_compare(key,(*p)->key);
-		if (x < 0) 
+	while (NULL != (*p = distill(*p))) {
+		const int x = gale_data_compare(key,(*p)->key);
+		if (x < 0)
 			p = &(*p)->left;
 		else if (x > 0)
 			p = &(*p)->right;
@@ -42,18 +63,19 @@ void gale_map_add(struct gale_map *wt,struct gale_data key,void *data) {
 	if (NULL != data) gale_create(new); /* This must happen first! */
 	p = find(wt,key);
 
-	if (NULL != *p) new = *p;
-	else if (NULL == data) return;
+	if (NULL != *p) 
+		new = *p;
+	else if (NULL == data) 
+		return;
 	else {
-		*p = new;
 		new->key = key;
 		new->left = NULL;
 		new->right = NULL;
 	}
 
-	if (NULL == data) new->data = NULL;
-	else if (wt->weak) new->data = gale_make_weak(data);
-	else new->data = gale_make_ptr(data);
+	new->data = (NULL == data) ? NULL
+	          : (wt->weak ? gale_make_weak : gale_make_ptr)(data);
+	*p = distill(new);
 }
 
 void *gale_map_find(const struct gale_map *wt,struct gale_data key) {
@@ -61,45 +83,32 @@ void *gale_map_find(const struct gale_map *wt,struct gale_data key) {
 	return n ? gale_get_ptr(n->data) : NULL;
 }
 
-static int empty(const struct wt_node *node) {
-	return (NULL == node->right
-            &&  NULL == node->left
-	    && (NULL == node->data || NULL == gale_get_ptr(node->data)));
-}
-
-static int walk(struct wt_node *node,const struct gale_data *after,
-	        struct gale_data *key,void **data) 
+static int walk(
+	struct wt_node *node,
+	const struct gale_data *after,
+	struct gale_data *key,void **data) 
 {
-	int x = after ? gale_data_compare(*after,node->key) : -1;
+	int x;
+	if (NULL == node) return 0;
+	x = after ? gale_data_compare(*after,node->key) : -1;
 
-	if (x < 0 && NULL != node->left) {
+	if (x < 0) {
+		node->left = distill(node->left);
 		if (walk(node->left,after,key,data)) return 1;
-		if (empty(node->left)) node->left = NULL;
+		if (NULL != data) *data = gale_get_ptr(node->data);
+		if (NULL != key) *key = node->key;
+		return 1;
 	}
 
-	if (x < 0 && NULL != node->data) {
-		void *ptr = gale_get_ptr(node->data);
-		if (NULL != data) *data = ptr;
-		if (NULL == ptr) node->data = NULL;
-		else {
-			if (NULL != key) *key = node->key;
-			return 1;
-		}
-	}
-
-	if (NULL != node->right) {
-		if (x <= 0) after = NULL;
-		if (walk(node->right,after,key,data)) return 1;
-		if (empty(node->right)) node->right = NULL;
-	}
-
-	return 0;
+	node->right = distill(node->right);
+	return walk(node->right,after,key,data);
 }
 
-int gale_map_walk(const struct gale_map *wt,const struct gale_data *after,
-                  struct gale_data *key,void **data) {
-	if (NULL == wt->root) return 0;
-	if (walk(wt->root,after,key,data)) return 1;
-	if (empty(wt->root)) ((struct gale_map *) wt)->root = NULL;
-	return 0;
+int gale_map_walk(
+	const struct gale_map *wt,
+	const struct gale_data *after,
+	struct gale_data *key,void **data) 
+{
+	((struct gale_map *) wt)->root = distill(wt->root);
+	return walk(wt->root,after,key,data);
 }
